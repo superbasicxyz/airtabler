e0279437ee8c2b91a6469d31161dbdd90b1bbe5e
James Pierce <james@superbasic.xyz>
2022-03-23 12:18:49 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multiply = void 0;
+function multiply(a, b) {
+    return a * b;
+}
+exports.multiply = multiply;
