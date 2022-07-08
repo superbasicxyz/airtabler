f9cde23eb77e15d97952d6b6c4e4ecc3bf1a9532
James Pierce <james@superbasic.xyz>
2022-07-08 17:04:47 -0500

    fix airtablerRequest

--- src/airtablerRequest.ts
+++ src/airtablerRequest.ts
@@ -4,7 +4,7 @@ import { AirtablerConfig } from "./types.d";
 export function airtablerRequest(url: string, { apiKey }: AirtablerConfig): Promise<AxiosResponse> {
   return axios.get(url, {
     headers: {
-      Authorization: apiKey
+      Authorization: `Bearer ${apiKey}`
     }
   });
 }
