728c5936a50456e6777af13392e66485e0ad902e
James Pierce <james@superbasic.xyz>
2022-08-15 20:32:45 -0500

    run build

--- dist/airtablerRequest.js
+++ dist/airtablerRequest.js
@@ -8,13 +8,20 @@ const axios_1 = __importDefault(require("axios"));
 const defaultOptions = {
     method: "get"
 };
-function airtablerRequest(url, { apiKey }, { method } = defaultOptions) {
-    return (0, axios_1.default)({
+function airtablerRequest(url, { apiKey }, options) {
+    const method = options ? options.method : defaultOptions.method;
+    const data = (options === null || options === void 0 ? void 0 : options.data) ? options.data : null;
+    const requestConfig = {
         method,
         url,
         headers: {
             Authorization: `Bearer ${apiKey}`
         }
-    });
+    };
+    if (method == "post" && data) {
+        requestConfig.headers["Content-Type"] = "application/json";
+        requestConfig.data = data;
+    }
+    return (0, axios_1.default)(requestConfig);
 }
 exports.airtablerRequest = airtablerRequest;
--- dist/model/index.js
+++ dist/model/index.js
@@ -6,6 +6,7 @@ const all_1 = require("./all");
 const find_1 = require("./find");
 const where_1 = require("./where");
 const destroy_1 = require("./destroy");
+const create_1 = require("./create");
 function model(config) {
     return (tableName) => {
         const tableUrl = new URL(`${(0, baseUrl_1.baseUrl)(config.baseId)}/${tableName}`);
@@ -15,7 +16,8 @@ function model(config) {
             all: (0, all_1.all)(tableUrl, config),
             find: (0, find_1.find)(tableUrl, config),
             where: (0, where_1.where)(tableUrl, config),
-            destroy: (0, destroy_1.destroy)(tableUrl, config)
+            destroy: (0, destroy_1.destroy)(tableUrl, config),
+            create: (0, create_1.create)(tableUrl, config)
         };
     };
 }
