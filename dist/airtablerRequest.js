25626076d6952b676386441c1a2d0ddf261aec26
James Pierce <james@superbasic.xyz>
2022-08-31 15:24:55 -0500

    run build

--- dist/airtablerRequest.js
+++ dist/airtablerRequest.js
@@ -8,9 +8,7 @@ const axios_1 = __importDefault(require("axios"));
 const defaultOptions = {
     method: "get"
 };
-function airtablerRequest(url, { apiKey }, options) {
-    const method = options ? options.method : defaultOptions.method;
-    const data = (options === null || options === void 0 ? void 0 : options.data) ? options.data : null;
+function airtablerRequest(url, { apiKey }, { method, data } = defaultOptions) {
     const requestConfig = {
         method,
         url,
@@ -18,10 +16,34 @@ function airtablerRequest(url, { apiKey }, options) {
             Authorization: `Bearer ${apiKey}`
         }
     };
-    if ((method == "post" || method == "patch") && data) {
-        requestConfig.headers["Content-Type"] = "application/json";
-        requestConfig.data = data;
+    switch (method) {
+        case "get":
+            return airtablerGetRequest(requestConfig);
+            break;
+        case "post":
+            return airtablerPostRequest(requestConfig, data);
+            break;
+        case "patch":
+            return airtablerPatchRequest(requestConfig, data);
+            break;
+        case "delete":
+            return airtablerDeleteRequest(requestConfig);
+            break;
+        default:
+            console.error(`unsupported request method: ${method}`);
+            break;
     }
     return (0, axios_1.default)(requestConfig);
 }
 exports.airtablerRequest = airtablerRequest;
+function airtablerGetRequest(requestConfig) {
+    return (0, axios_1.default)(requestConfig);
+}
+function airtablerPostRequest(requestConfig, data) {
+    return (0, axios_1.default)(Object.assign(Object.assign({}, requestConfig), { headers: Object.assign(Object.assign({}, requestConfig.headers), { "Content-Type": "application/json" }), data }));
+}
+// the functions below take the same arguments and handle the request the same way
+// we'll put them in as aliases for now. if we need to break them out with their own
+// logic we can easily do so later
+const airtablerPatchRequest = airtablerPostRequest;
+const airtablerDeleteRequest = airtablerGetRequest;
