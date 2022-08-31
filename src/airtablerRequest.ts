76fa810c4634aee2a2b8b98282af90a6e825df83
James Pierce <james@superbasic.xyz>
2022-08-31 14:48:59 -0500

    refactor `airtablerRequest`

--- src/airtablerRequest.ts
+++ src/airtablerRequest.ts
@@ -2,22 +2,19 @@ import axios, { AxiosResponse, Method, AxiosRequestConfig } from "axios";
 import { AirtablerConfig } from "../types.d";
 
 interface AirtablerRequestOptions {
-  method: Method,
-  data?: any
+  method: Method;
+  data?: any;
 }
 
 const defaultOptions: AirtablerRequestOptions = {
   method: "get"
-}
+};
 
 export function airtablerRequest(
   url: string,
   { apiKey }: AirtablerConfig,
-  options?: AirtablerRequestOptions
+  { method, data }: AirtablerRequestOptions = defaultOptions
 ): Promise<AxiosResponse> {
-  const method = options ? options.method : defaultOptions.method;
-  const data = options?.data ? options.data : null;
-
   const requestConfig: AxiosRequestConfig = {
     method,
     url,
@@ -26,10 +23,57 @@ export function airtablerRequest(
     }
   };
 
-  if ((method == "post" || method == "patch") && data) {
-    requestConfig.headers!["Content-Type"] = "application/json";
-    requestConfig.data = data;
+  switch (method) {
+    case "get":
+      return airtablerGetRequest(requestConfig);
+      break;
+    case "post":
+      return airtablerPostRequest(requestConfig, data);
+      break;
+    case "patch":
+      return airtablerPatchRequest(requestConfig, data);
+      break;
+    case "delete":
+      return airtablerDeleteRequest(requestConfig);
+      break;
+    default:
+      console.error(`unsupported request method: ${method}`);
+      break;
   }
 
+  return axios(requestConfig);
+}
+
+function airtablerGetRequest(
+  requestConfig: AxiosRequestConfig
+): Promise<AxiosResponse> {
+  return axios(requestConfig);
+}
+
+function airtablerPostRequest(
+  requestConfig: AxiosRequestConfig,
+  data: any
+): Promise<AxiosResponse> {
+  return axios({
+    ...requestConfig,
+    headers: { ...requestConfig.headers, "Content-Type": "application/json" },
+    data
+  });
+}
+
+function airtablerPatchRequest(
+  requestConfig: AxiosRequestConfig,
+  data: any
+): Promise<AxiosResponse> {
+  return axios({
+    ...requestConfig,
+    headers: { ...requestConfig.headers, "Content-Type": "application/json" },
+    data
+  });
+}
+
+function airtablerDeleteRequest(
+  requestConfig: AxiosRequestConfig
+): Promise<AxiosResponse> {
   return axios(requestConfig);
 }
